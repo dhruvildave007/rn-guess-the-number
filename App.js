f8304cb7478aace3efd71b05608b2b21c5d3f69e
Dhruvil Dave <dhruvil@gkmit.co>
2020-07-07 12:54:56 +0530

    screen folder name updated

--- App.js
+++ App.js
@@ -1,34 +1,47 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
 import Header from './components/Header';
-import StartGameScreen from './sreens/StartGameScreen';
-import GameScreen from './sreens/GameScreen';
-import GameOverScreen from './sreens/GameOverScreen';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber)
+  const configureNewGameHandler = () => {
     setGuessRounds(0);
+    setUserNumber(null);
   };
 
-  const gameOverHandler = numberOfRounds => {
-    setUserNumber(numberOfRounds);
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
   if (userNumber && guessRounds <= 0) {
-    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-  } else if(guessRounds > 0) {
-    content = <GameOverScreen />
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
     <View style={styles.screen}>
-      <Header title="Geuss the Number" />
+      <Header title="Guess a Number" />
       {content}
     </View>
   );
@@ -36,6 +49,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
+    flex: 1
   }
 });
